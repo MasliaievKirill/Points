7e96b33b98348b89e01912978333039049b51ab4
MasliaievKirill <kirillmaslyaev@gmail.com>
2022-09-25 21:03:34 +0300

    feature/PTS-003 Changed compile and target version. Added Google map.

--- app/src/main/java/com/masliaiev/points/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/masliaiev/points/presentation/screens/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.masliaiev.points.presentation.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.masliaiev.points.presentation.theme.PointsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +32,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppGoogleMap()
                 }
             }
         }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return if (
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            true
+        } else {
+            requestPermissions()
+            false
+        }
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ACCESS_FINE_LOCATION_CODE
+        )
+
+    }
+
+    companion object {
+        private const val PERMISSION_ACCESS_FINE_LOCATION_CODE = 1
     }
 }
 
@@ -34,10 +70,34 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun AppGoogleMap() {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "My position",
+            snippet = "Marker in position"
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PointsTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AppGoogleMap()
+        }
     }
 }
